aafba04666297c228fb2b664d76807b1bcc91510
Bianchini <juan.bianchini@firstdata.com>
2023-12-11 09:51:32 -0300

    initial commit

---
+++ src/main/java/com/fiserv/chargebackapi/ChargebackApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiserv.chargebackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargebackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargebackApiApplication.class, args);
+	}
+
+}
